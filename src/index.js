539462d334825f6ea8aa1497160bb1c7e48195f0
daiki-skm <daikisakuma6040@gmail.com>
2023-03-09 21:10:31 +0900

    wip

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const fsPromises = require("fs/promises");
 
-const filePath = path.resolve(__dirname, "./json/invoices.json");
+const jsonFile = ["invoices.json", "plays.json"];
+const filePath = path.resolve(__dirname, "./json/");
 
 const main = async () => {
   try {
-    const data = await fsPromises.readFile(filePath);
+    const data = await fsPromises.readFile(filePath + jsonFile[0]);
     const invoices = JSON.parse(data);
     console.log(invoices);
   } catch (err) {
