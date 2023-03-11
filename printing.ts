48a19bb779b0ca00adcecfba089f240538b16f63
daiki-skm <daikisakuma6040@gmail.com>
2023-03-12 08:48:37 +0900

    fix: inline

--- printing.ts
+++ printing.ts
@@ -39,16 +39,14 @@ export const statement = (invoice: Invoice, plays: Plays) => {
   };
 
   for (const perf of invoice.performances) {
-    let thisAmount = amountFor(perf);
-
     volumeCredits += Math.max(perf.audience - 30, 0);
     if ("comedy" === playFor(perf).type) {
       volumeCredits += Math.floor(perf.audience / 5);
     }
-    result += `${playFor(perf).name}: ${format(thisAmount / 100)} (${
+    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
-    totalAmount += thisAmount;
+    totalAmount += amountFor(perf);
   }
 
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
