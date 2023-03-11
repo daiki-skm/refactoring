e3be4df4835385d18a58755c4a5f54d57a2f904d
daiki-skm <daikisakuma6040@gmail.com>
2023-03-11 14:50:56 +0900

    wip

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 import { statement } from "./printing.ts";
+import type { Invoice } from "./types/invoice.ts";
+import type { Plays } from "./types/plays.ts";
 
 if (import.meta.main) {
   const invoiceJson = await Deno.readTextFile("./json/invoice.json");
-  const invoice = JSON.parse(invoiceJson);
+  const invoice: Invoice = JSON.parse(invoiceJson);
   const playsJson = await Deno.readTextFile("./json/plays.json");
-  const plays = JSON.parse(playsJson);
+  const plays: Plays = JSON.parse(playsJson);
   const result = statement(invoice, plays);
   console.log(result);
 }
