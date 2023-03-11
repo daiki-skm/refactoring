895fa41eb2931ab121b776ceaa04953ca6c819ac
daiki-skm <daikisakuma6040@gmail.com>
2023-03-12 08:45:07 +0900

    fix: delete a vari

--- printing.ts
+++ printing.ts
@@ -17,14 +17,13 @@ export const statement = (invoice: Invoice, plays: Plays) => {
   };
 
   for (const perf of invoice.performances) {
-    const play = playFor(perf);
-    let thisAmount = amountFor(perf, play);
+    let thisAmount = amountFor(perf, playFor(perf));
 
     volumeCredits += Math.max(perf.audience - 30, 0);
-    if ("comedy" === play.type) {
+    if ("comedy" === playFor(perf).type) {
       volumeCredits += Math.floor(perf.audience / 5);
     }
-    result += `${play.name}: ${format(thisAmount / 100)} (${
+    result += `${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += thisAmount;
