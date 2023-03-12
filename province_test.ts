3bf5810c6c1625c3bc8c5d64666ffa1531fe9d93
daiki-skm <daikisakuma6040@gmail.com>
2023-03-12 21:48:12 +0900

    fix: add a test

--- province_test.ts
+++ province_test.ts
@@ -5,3 +5,8 @@ Deno.test("province shortfall", () => {
   const asia = new Province(sampleProvinceData());
   assertEquals(asia.shortfall, 5);
 });
+
+Deno.test("province profit", () => {
+  const asia = new Province(sampleProvinceData());
+  assertEquals(asia.profit, 230);
+});
