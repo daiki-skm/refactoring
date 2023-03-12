55305fd2fe77b03fc77b7c5ce93611d5dab3cc1c
daiki-skm <daikisakuma6040@gmail.com>
2023-03-12 10:15:09 +0900

    fix: divide func

--- printing.ts
+++ printing.ts
@@ -45,12 +45,17 @@ export const statement = (invoice: Invoice, plays: Plays) => {
     }).format(number / 100);
   };
 
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
+  const totalVolumeCredits = () => {
+    let result = 0;
+    for (const perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  };
 
+  let totalAmount = 0;
+  let result = `Statement for ${invoice.customer}\n`;
   for (const perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
@@ -58,6 +63,6 @@ export const statement = (invoice: Invoice, plays: Plays) => {
   }
 
   result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 };
