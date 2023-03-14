8af8335ef9e59c0e8bfc124728a5fa13c8abfd94
daiki-skm <daikisakuma6040@gmail.com>
2023-03-14 21:00:20 +0900

    wip

--- rating.ts
+++ rating.ts
@@ -11,9 +11,7 @@ const reportLines = (aCustomer) => {
 
 const price = (order) => {
   const basePrice = order.quantity * order.itemPrice;
-  return (
-    basePrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(basePrice * 0.1, 100)
-  );
+  const quantityDiscount =
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  return basePrice - quantityDiscount + Math.min(basePrice * 0.1, 100);
 };
