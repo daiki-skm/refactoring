3046955bfcd9ce48feb003a687a1193940c83521
daiki-skm <daikisakuma6040@gmail.com>
2023-03-14 20:44:22 +0900

    wip

--- rating.ts
+++ rating.ts
@@ -1,3 +1,14 @@
 const rating = (aDriver) => {
   return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
 };
+
+const reportLines = (aCustomer) => {
+  const lines = [];
+  gatherCustomerData(lines, aCustomer);
+  return lines;
+};
+
+const gatherCustomerData = (out, aCustomer) => {
+  out.push(["name", aCustomer.name]);
+  out.push(["location", aCustomer.location]);
+};
