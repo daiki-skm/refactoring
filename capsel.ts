4b0a77454c22cf16456729d36c0bdea168dfdee7
daiki-skm <daikisakuma6040@gmail.com>
2023-03-17 05:44:01 +0900

    wip

--- capsel.ts
+++ capsel.ts
@@ -3,9 +3,22 @@ spaceship.owner = defaultOwner;
 setDefaultOwner({ firstName: "test", lastName: "test" });
 
 function defaultOwner() {
-  return Object.assign({}, defaultOwnerData);
+  return new Person(defaultOwnerData);
 }
 
 function setDefaultOwner(arg) {
   defaultOwnerData = arg;
+}
+
+class Person {
+  constructor(data) {
+    this._lastName = data.lastName;
+    this._firstName = data.firstName;
+  }
+  get lastName() {
+    return this._lastName;
+  }
+  get firstName() {
+    return this._firstName;
+  }
 }
